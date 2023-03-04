f8db052f591b1f53d52dc15e166f4ab972b40f7b
Govinda Mandal <govinda4india@gmail.com>
2023-03-04 21:43:25 +0530

    App setup and CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import styles from './style'
 
 const App = () => (
     <div className="bg-primary w-full overflow-hidden">
-      <h1>Hello World!</h1>
+      <div className={`${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Navbar
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Hero
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Stats
+          Business
+          Billings
+          CardDeal
+          Testimonials
+          Clients
+          CTA
+          Footer
+        </div>
+      </div>
     </div>
   )
 
